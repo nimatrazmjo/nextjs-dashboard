58760ebc409fcfe410a78166e55c042093e3d0df
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2023-12-12 11:29:22 -0500

    Connected to postgres sql

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,36 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { sql } from '@vercel/postgres';
 import Link from 'next/link';
 
-export default function Page() {
+console.log({POSTGRES_URL: process.env.POSTGRES_URL, POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING}, "process.env.POSTGRES_URL_NON_POOLING");
+
+export default async function Page({params}: {params: {user: string}}) {
+
+  const { rows }= await sql`SELECT * FROM users WHERE username = ${params.user}`;
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+      <h1 className="text-3xl font-bold">Welcome to Acme!</h1>
+      <p className="text-lg">This is a demo app for <a href="https://vercel.com">Vercel</a> and <a href="https://nextjs.org">Next.js</a>.</p>
+      <p className="text-lg">You can find the full source code on </p>
+
+      {rows.length > 0 ? (
+        <div className="flex flex-col mt-6">
+          <h2 className="text-xl font-bold">Users</h2>
+          <ul className="flex flex-col mt-2">
+            {rows.map((row) => (
+              <li key={row.id} className="flex items-center">
+                <Link href={`/user/${row.username}`}>
+                  <a className="text-blue-500 hover:underline">{row.username}</a>
+                </Link>
+                <ArrowRightIcon className="w-4 h-4 text-gray-500" />
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-        </div>
-      </div>
+      ) : (
+        <p className="mt-6 text-lg">No users found.</p>
+      )}
     </main>
   );
 }
